efe38782190d4dc67568644b7efc4cc5807adf09
Ilya Salanovich <ilyasolonovich20@gmail.com>
2024-03-11 21:26:48 +0100

    update linter tests

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ class BasicValidator {
   }
 
   isValid(value) {
-    if(!this.requiredCalled && !value) return true;
+    if (!this.requiredCalled && !value) return true;
 
     return this.validationFuncs
       .every((validationFunc) => validationFunc(value));
@@ -25,17 +25,17 @@ class StringValidator extends BasicValidator {
   required() {
     const requiredCheck = (val) => !!val && typeof val === 'string';
     this.requiredCalled = true;
-    return this.addDefaultValidator(requiredCheck)
+    return this.addDefaultValidator(requiredCheck);
   }
 
   contains(string) {
     const containsCheck = (val) => val.includes(string);
-    return this.addDefaultValidator(containsCheck)
+    return this.addDefaultValidator(containsCheck);
   }
 
   minLength(length) {
     const minLengthCheck = (val) => val.length >= length;
-    return this.addDefaultValidator(minLengthCheck)
+    return this.addDefaultValidator(minLengthCheck);
   }
 }
 
@@ -53,7 +53,7 @@ class NumberValidator extends BasicValidator {
 
   range(rangeStart, rangeEnd) {
     const rangeCheck = (val) => (val >= rangeStart && val <= rangeEnd);
-    return this.addDefaultValidator(rangeCheck)
+    return this.addDefaultValidator(rangeCheck);
   }
 }
 
@@ -61,32 +61,21 @@ class ArrayValidator extends BasicValidator {
   required() {
     const requiredCheck = (val) => Array.isArray(val);
     this.requiredCalled = true;
-    return this.addDefaultValidator(requiredCheck)
+    return this.addDefaultValidator(requiredCheck);
   }
 
   sizeof(length) {
     const sizeOfArrayCheck = (val) => val.length === length;
-    return this.addDefaultValidator(sizeOfArrayCheck)
+    return this.addDefaultValidator(sizeOfArrayCheck);
   }
 }
 
 class HashValidator extends BasicValidator {
   shape(hash) {
-    const shapeCheck = (val) => {
-      const recursiveShapeCheck = (checkedValue, shapeForm) => {
-        return Object.entries(shapeForm).every(([key, value]) => {
-          // if (value.constructor === Object) {
-          //   return shapeCheck(checkedValue[key], shapeForm[key])
-          // }
-          return value.isValid(checkedValue[key]);
-        })
-      }
-
-      return recursiveShapeCheck(val, hash)
-    }
+    const shapeChk = (val) => Object.entries(hash).every(([key, value]) => value.isValid(val[key]));
 
     this.requiredCalled = true;
-    return this.addDefaultValidator(shapeCheck);
+    return this.addDefaultValidator(shapeChk);
   }
 }
 
@@ -95,13 +84,13 @@ const validatorClassesMapping = {
   number: NumberValidator,
   array: ArrayValidator,
   hash: HashValidator,
-}
+};
 
 export default class Validator {
   constructor() {
     Object.keys(validatorClassesMapping).forEach((key) => {
-      this[`type${key}`] = {customValidators: {}};
-    })
+      this[`type${key}`] = { customValidators: {} };
+    });
   }
 
   addValidator(type, name, fn) {
@@ -110,26 +99,25 @@ export default class Validator {
 
   string() {
     const validator = new StringValidator();
-    validator.testValidationFuncs = this['typestring'].customValidators;
+    validator.testValidationFuncs = this.typestring.customValidators;
     return validator;
   }
 
   number() {
     const validator = new NumberValidator();
-    validator.testValidationFuncs = this['typenumber'].customValidators;
+    validator.testValidationFuncs = this.typenumber.customValidators;
     return validator;
   }
 
   array() {
     const validator = new ArrayValidator();
-    validator.testValidationFuncs = this['typearray'].customValidators;
+    validator.testValidationFuncs = this.typearray.customValidators;
     return validator;
   }
 
   object() {
     const validator = new HashValidator();
-    validator.testValidationFuncs = this['typehash'].customValidators;
+    validator.testValidationFuncs = this.typehash.customValidators;
     return validator;
   }
 }
-
